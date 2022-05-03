82b962f6448c90e36c53d7b7975ceb545eb0677c
thaiduong1705 <thaiduong7v@gmail.com>
2022-05-03 16:17:51 +0700

    Them bao cao doanh so

--- QuanLyGaraOto/ViewModel/MainViewModel.cs
+++ QuanLyGaraOto/ViewModel/MainViewModel.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace QuanLyGaraOto.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
-        private BaseViewModel _selectedViewModel = new SwitchViewCarCheckIn();
+        private BaseViewModel _selectedViewModel = new SwitchViewSalesReport();
 
         public BaseViewModel SelectedViewModel 
         { 
@@ -21,10 +23,57 @@ namespace QuanLyGaraOto.ViewModel
                 OnPropertyChanged(nameof(SelectedViewModel));
             }
         }
+        
+        public ICommand SelectViewCommand { get; set; }
 
         public MainViewModel()
         {
-            
+            SelectViewCommand = new RelayCommand<ListViewItem>((p) => { return true; }, (p) => { SelectView(p); });
+        }
+
+        void SelectView(ListViewItem listViewItem)
+        {
+            if (listViewItem != null && listViewItem.Tag != null)
+            {
+                string tag = listViewItem.Tag.ToString();
+                
+                switch(tag)
+                {
+                    case "CarCheckIn":
+                        { 
+                            SelectedViewModel = new SwitchViewCarCheckIn();
+                            break;
+                        }
+                    case "RepairReceipt":
+                        {
+                            break;
+                        }
+                    case "Bill":
+                        {
+                            break;
+                        }
+                    case "Search":
+                        {
+                            break;
+                        }
+                    case "SalesReport":
+                        {
+                            SelectedViewModel = new SwitchViewSalesReport();
+                            break;
+                        }
+                    case "AccessoriesReport":
+                        {
+                            break;
+                        }
+                    case "AddAccessoriesReport":
+                        {
+                            break;
+                        }
+                    default:
+                        break;
+                }    
+                
+            }    
         }
     }
 }
