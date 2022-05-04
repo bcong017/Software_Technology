f5459e43ab657de862d0e81cf182d62e2a268436
thaiduong1705 <thaiduong7v@gmail.com>
2022-05-04 12:18:33 +0700

    them form bao cao doanh so

--- QuanLyGaraOto/ViewModel/SalesReportViewModel.cs
+++ QuanLyGaraOto/ViewModel/SalesReportViewModel.cs
@@ -8,9 +8,10 @@ namespace QuanLyGaraOto.ViewModel
 {
     public class SalesReportViewModel : BaseViewModel
     {
+        public List<int> Month { get; set; }
         public SalesReportViewModel()
         {
-
+            Month = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
         }
     }
 }
