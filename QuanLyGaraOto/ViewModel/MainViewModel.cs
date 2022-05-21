c504649b2b47a43272ec9fe185a1fbe2bd49eb1a
thaiduong1705 <thaiduong7v@gmail.com>
2022-05-21 14:59:44 +0700

    them ui phieu sua chua

--- QuanLyGaraOto/ViewModel/MainViewModel.cs
+++ QuanLyGaraOto/ViewModel/MainViewModel.cs
@@ -69,6 +69,7 @@ namespace QuanLyGaraOto.ViewModel
                         }
                     case "RepairReceipt":
                         {
+                            SelectedViewModel = new SwitchViewRepairReceipt();
                             break;
                         }
                     case "Bill":
