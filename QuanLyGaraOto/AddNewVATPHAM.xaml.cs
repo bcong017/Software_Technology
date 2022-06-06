6e15b7ed12cbe0c92880249c38dd2a541f7e5a06
Wibuman <tgs.joker@gmail.com>
2022-06-06 19:57:47 +0700

    hoàn thiện repairrecept no database

---
+++ QuanLyGaraOto/AddNewVATPHAM.xaml.cs
@@ -0,0 +1,34 @@
+﻿using QuanLyGaraOto.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyGaraOto
+{
+    public partial class AddNewVATPHAM : Window
+    {
+        public AddNewVATPHAM(RepairReceiptViewModel repairReceiptViewModel)
+        {
+            InitializeComponent();
+            this.DataContext = new AddNewVATPHAMViewModel(repairReceiptViewModel);
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+    }
+}
