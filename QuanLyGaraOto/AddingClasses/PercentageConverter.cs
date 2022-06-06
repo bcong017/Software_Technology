6fb6738b6bfbaf9bf63ecdef0c493b94871fdbf7
thaiduong1705 <thaiduong7v@gmail.com>
2022-06-07 02:13:10 +0700

    fix tham so

---
+++ QuanLyGaraOto/AddingClasses/PercentageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace QuanLyGaraOto.AddingClasses
+{
+    public class PercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fraction = decimal.Parse(value.ToString());
+            return fraction.ToString("P2");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Trim any trailing percentage symbol that the user MAY have included
+            var valueWithoutPercentage = value.ToString().TrimEnd(' ', '%');
+            return decimal.Parse(valueWithoutPercentage) / 100;
+        }
+    }
+}
--- QuanLyGaraOto/ViewModel/UpdateParameterViewModel.cs
+++ QuanLyGaraOto/ViewModel/UpdateParameterViewModel.cs
@@ -8,6 +8,20 @@ namespace QuanLyGaraOto.ViewModel
 {
     public class UpdateParameterViewModel : BaseViewModel
     {
+        private decimal percentage;
+        public decimal Percentage
+        {
+            get { return percentage; }
+            set { percentage = value; OnPropertyChanged(nameof(Percentage)); }
+        }
+
+        private int carNumber;
+        public int CarNumber
+        {
+            get { return carNumber; }
+            set { carNumber = value; OnPropertyChanged(nameof(CarNumber)); }
+        }
+
         public UpdateParameterViewModel()
         {
 
