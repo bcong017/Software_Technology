ff7f77fb290ca8b88aba41a1db66c8d137806cae
The Star Gazer <20520417@gm.uit.edu.vn>
2022-06-09 00:08:01 +0700

    Hoàn thiện chức năng lập phiếu thu tiền

---
+++ QuanLyGaraOto/Model/BillsNumbericalOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyGaraOto.Model
+{
+    public class BillsNumbericalOrder
+    {
+        public int Number { get; set; }
+        public PHIEUTHUTIEN PhieuThuTien { get; set; }
+    }
+}
